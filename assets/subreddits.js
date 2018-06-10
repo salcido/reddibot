f7eebd46398f2402c22f17766bb901411be901f4
Matthew Salcido <salcido@users.noreply.github.com>
2018-06-10 11:37:07 -0700

    move subs to own file

---
+++ assets/subreddits.js
@@ -0,0 +1,35 @@
+// =======================================================
+//  Overview
+//  -----------------------------------------------------
+//  A list of subreddits the bot will pull posts from.
+//  -----------------------------------------------------
+//  @author: Matthew Salcido
+//  @github: https://www.github.com/salcido
+//  @source: https://github.com/salcido/reddibot
+//  @bot-url: https://www.twitter.com/reddibot
+// =======================================================
+const subreddits = {
+  subs: [
+    'animalsbeingderps',
+    'aww',
+    'awwducational',
+    'coloringcorruptions',
+    'engineeringporn',
+    'expectationvsreality',
+    'eyebleach',
+    'ilikthebred',
+    'imaginarylandscapes',
+    'natureisfuckinglit',
+    'nocontext',
+    'perfectfit',
+    'rarepuppers',
+    'showerthoughts',
+    'spaceporn',
+    'superbowl',
+    'whatswrongwithyourdog',
+    'woahdude'
+  ],
+  textSubs: ['nocontext', 'showerthoughts']
+};
+
+exports.subreddits = subreddits;
